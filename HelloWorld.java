02653be0c83f19b3c0c9bf2ad89b1cc3608b3f12
Michealia <michealia092006@gmail.com>
2024-09-27 10:03:20 +0700

    michealia

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
     public static void main(String[]args){
-        System.out.println("hallo");
+       System.out.println("miselll");
+       System.out.println("nama");
+       System.out.println("haii");
     }
 }
